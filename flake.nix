2b536a1b7df7bdbc19c7b7ad27f04e410218e602
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-11-13 11:05:54 +0200

    reorganize/format flake

--- flake.nix
+++ flake.nix
@@ -1,81 +1,78 @@
 {
-  description = "Petri's NixOS";
+    description = "Petri's Nixos configurations";
 
-  inputs = {
+    inputs = {
 
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+        nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+        flake-utils.url = "github:numtide/flake-utils";
 
-    darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Use my dotfiles as input
-    dotfiles = {
-      url = "github:plehton/dotfiles";
-      flake = false;
-    };
-
-  };
-
-  outputs = inputs@{ self, nixpkgs, home-manager, nixos-wsl, darwin, ... }:
-    let
-      lib = nixpkgs.lib;
-      user = "pjl";
-      userName = "Petri Lehtonen";
-    in {
-
-      # Macbook
-      darwinConfigurations.MV9J7YK4N9 = darwin.lib.darwinSystem rec {
-        specialArgs = {
-          inherit user;
-          inherit inputs;
-          inherit self;
+        darwin = {
+            url = "github:LnL7/nix-darwin";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
-        modules = [
-          ./hosts/macbook
-          home-manager.darwinModules.home-manager {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = specialArgs;
-              backupFileExtension = "oli";
-              users.${user}.imports = [ ./home ];
-            };
-          }
-        ];
-      };
 
-      # UTM VM
-      nixosConfigurations.utmos= lib.nixosSystem rec {
-        pkgs = nixpkgs.legacyPackages.aarch64-linux;
-        specialArgs = {
-          inherit self;
-          inherit inputs;
-          inherit user;
-          inherit userName;
+        nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
-        system = "aarch64-linux";
-        modules = [
-          ./hosts/utm
-          .home-manager.nixosModules.home-manager {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "oli";
-              users.${user}.imports = [ ./home ];
-              extraSpecialArgs = specialArgs;
-            };
-          }
-        ];
-      };
 
+        dotfiles = {
+            url = "github:plehton/dotfiles";
+            flake = false;
+        };
     };
+
+    outputs = inputs@{ self, nixpkgs, flake-utils, home-manager, nixos-wsl, darwin, ... }:
+        let
+            lib = nixpkgs.lib;
+            user = "pjl";
+            userName = "Petri Lehtonen";
+        in {
+
+            # Macbook
+            darwinConfigurations.MV9J7YK4N9 = darwin.lib.darwinSystem rec {
+                specialArgs = {
+                    inherit user;
+                    inherit inputs;
+                    inherit self;
+                };
+                modules = [
+                    ./hosts/macbook
+                    home-manager.darwinModules.home-manager {
+                      home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        extraSpecialArgs = specialArgs;
+                        users.${user}.imports = [ ./home ];
+                      };
+                    }
+                ];
+            };
+
+            # UTM VM
+            nixosConfigurations.utmos= lib.nixosSystem rec {
+                pkgs = nixpkgs.legacyPackages.aarch64-linux;
+                specialArgs = {
+                    inherit self;
+                    inherit inputs;
+                    inherit user;
+                    inherit userName;
+                };
+                system = "aarch64-linux";
+                modules = [ ./hosts/utm
+                    .home-manager.nixosModules.home-manager {
+                      home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        users.${user}.imports = [ ./home ];
+                        extraSpecialArgs = specialArgs;
+                      };
+                    }
+                ];
+            };
+
+};
 }
