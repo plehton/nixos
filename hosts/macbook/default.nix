884a9d84906845806250a75abfa400ba643ef325
Petri Lehtonen <petri.j.lehtonen@gmail.com>
2024-11-13 11:05:47 +0200

    add trusted users to macbook

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -1,38 +1,41 @@
 { pkgs, user, ... }:
 {
-	nixpkgs.hostPlatform = "aarch64-darwin";
-	services.nix-daemon.enable = true;
+    nixpkgs.hostPlatform = "aarch64-darwin";
+    services.nix-daemon.enable = true;
 
-	# Shell
-	environment.loginShell = pkgs.zsh;
-	environment.shells = [ pkgs.bash pkgs.zsh ];
-	programs.zsh.enable = true; # default shell on catalina
+    # Shell
+    environment.loginShell = pkgs.zsh;
+    environment.shells = [ pkgs.bash pkgs.zsh ];
+    programs.zsh.enable = true; # default shell on catalina
 
-	users.users.pjl = {
-          home = "/Users/${user}";
-          packages = with pkgs; [ ];
+    users.users.pjl = {
+        home = "/Users/${user}";
+        packages = with pkgs; [ ];
     };
 
-  environment.pathsToLink = [ "/share/zsh" ];
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-	];
+    environment.pathsToLink = [ "/share/zsh" ];
+    environment.systemPackages = with pkgs; [
+        git
+        vim
+    ];
 
-	# MacOS Settings
-	security.pam.enableSudoTouchIdAuth = true;
-	system.defaults = {
-		NSGlobalDomain.AppleShowAllExtensions = true;
-		NSGlobalDomain.InitialKeyRepeat = 20;
-		NSGlobalDomain.KeyRepeat = 2;
-		dock.autohide = true;
-		dock.mru-spaces = false;
-		finder.AppleShowAllExtensions = true;
-		finder._FXShowPosixPathInTitle = true;
-		screensaver.askForPasswordDelay = 60;
-	};
+    # MacOS Settings
+    security.pam.enableSudoTouchIdAuth = true;
+    system.defaults = {
+        NSGlobalDomain.AppleShowAllExtensions = true;
+        NSGlobalDomain.InitialKeyRepeat = 20;
+        NSGlobalDomain.KeyRepeat = 2;
+        dock.autohide = true;
+        dock.mru-spaces = false;
+        finder.AppleShowAllExtensions = true;
+        finder._FXShowPosixPathInTitle = true;
+        screensaver.askForPasswordDelay = 60;
+    };
 
-  nixpkgs.config.allowUnfree = true;
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
-	system.stateVersion = 5;
+    nixpkgs.config.allowUnfree = true;
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nix.settings.trusted-users = [ "root" user ];
+
+    system.stateVersion = 5;
 }
